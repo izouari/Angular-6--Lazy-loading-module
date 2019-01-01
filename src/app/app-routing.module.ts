ef91f054f87c06e536cbac54ce8c825af0dbdf34
Iheb ZOUARI <zouari.iheb@gmail.com>
2019-01-01 19:39:02 +0100

    add preLoadStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
 import {ProduitComponent} from "./produit/creation/produit.component";
 import {HomeComponent} from "./home/home.component";
 import {ProduitListComponent} from "./produit/list/produit-list.component";
@@ -13,7 +13,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
